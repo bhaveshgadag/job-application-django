b94c3a3cfe5fae878db4bc1be19446b4b5ef8773
bhaveshgadag <bhaveshgadag46@gmail.com>
2023-09-28 18:00:17 +0530

    message on form submission

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Form
+from django.contrib import messages
 
 
 def index(request):
@@ -16,5 +17,6 @@ def index(request):
             Form.objects.create(first_name=first_name, last_name=last_name,
                                 email=email, date=date, occupation=occupation)
 
+            messages.success(request, message="Form submitted successfully.")
 
     return render(request, "index.html")
